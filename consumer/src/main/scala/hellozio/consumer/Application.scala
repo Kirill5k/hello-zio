2bd8bb3c33884541e4b5342303d14a77ff2ba43d
kirill5k <immotional@aol.com>
2022-02-19 13:44:48 +0000

    Boostrap consumer application

--- consumer/src/main/scala/hellozio/consumer/Application.scala
+++ consumer/src/main/scala/hellozio/consumer/Application.scala
@@ -1,8 +1,20 @@
 package hellozio.consumer
 
+import hellozio.consumer.common.config.AppConfig
+import hellozio.consumer.todo.TodoConsumer
 import zio._
 
 object Application extends ZIOAppDefault {
+
+  val configLayer    = AppConfig.layer
+  val consumerLayer = (configLayer ++ Clock.live) >>> TodoConsumer.layer
+
   override def run: URIO[zio.ZEnv, ExitCode] =
-    Console.printLine("Hello, World!").orDie.exitCode
+    TodoConsumer
+      .updates
+      .mapZIO(u => ZIO.logInfo(s"Received update $u"))
+      .runDrain
+      .provideLayer(consumerLayer)
+      .orDie
+      .exitCode
 }
