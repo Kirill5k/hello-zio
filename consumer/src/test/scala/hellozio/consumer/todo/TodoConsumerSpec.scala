50786577ed6431be5acc94e1db3caca1090b6f38
kirill5k <immotional@aol.com>
2022-02-20 09:49:14 +0000

    Remove debug statements

--- consumer/src/test/scala/hellozio/consumer/todo/TodoConsumerSpec.scala
+++ consumer/src/test/scala/hellozio/consumer/todo/TodoConsumerSpec.scala
@@ -5,13 +5,12 @@ import hellozio.domain.todo.{TodoUpdate, Todos}
 import io.circe.syntax._
 import io.circe.generic.auto._
 import io.github.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{Deserializer, Serializer, StringDeserializer, StringSerializer}
-import org.scalatest.Ignore
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import zio._
 
-@Ignore
 class TodoConsumerSpec extends AnyWordSpec with Matchers with EmbeddedKafka {
 
   val topic     = "todo-updates"
@@ -32,7 +31,7 @@ class TodoConsumerSpec extends AnyWordSpec with Matchers with EmbeddedKafka {
         val result = for {
           fib <- TodoConsumer.updates.timeout(10.seconds).runCollect.map(_.toList).fork
           _   <- ZIO.sleep(2.seconds)
-          _   <- ZIO.attempt(publishToKafka(topic, Todos.id.value, todo.asJson.noSpaces))
+          _   <- ZIO.attempt(publishToKafka(new ProducerRecord(topic, Todos.id.value, todo.asJson.noSpaces)))
           _   <- ZIO.sleep(2.seconds)
           res <- fib.join
         } yield res
