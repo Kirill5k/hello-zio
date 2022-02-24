44df061193d232a401ee27327e77946d5ad0b89f
kirill5k <immotional@aol.com>
2022-02-24 08:49:58 +0000

    Define TodoController test spec

--- api/src/test/scala/hellozio/api/todo/TodoControllerSpec.scala
+++ api/src/test/scala/hellozio/api/todo/TodoControllerSpec.scala
@@ -1,8 +1,51 @@
 package hellozio.api.todo
 
-class TodoControllerSpec extends ControllerSpec {
+import org.scalatestplus.mockito.MockitoSugar
 
-  "A TodoController" should {
+class TodoControllerSpec extends ControllerSpec with MockitoSugar {
 
+  "A TodoController" when {
+
+    "GET /api/todos" should {
+      "return all todos" in {
+        pending
+      }
+    }
+
+    "GET /api/todos/:id" should {
+      "find todo by id" in {
+        pending
+      }
+
+      "return 404 when todo does not exist" in {
+        pending
+      }
+    }
+
+    "POST /api/todos" should {
+      "create new todo" in {
+        pending
+      }
+    }
+
+    "PUT /api/todos/:id" should {
+      "update existing todo" in {
+        pending
+      }
+
+      "return 404 when todo does not exist" in {
+        pending
+      }
+    }
+
+    "DELETE /api/todos/:id" should {
+      "delete existing todo and return 204" in {
+        pending
+      }
+
+      "return 404 when todo does not exist" in {
+        pending
+      }
+    }
   }
 }
