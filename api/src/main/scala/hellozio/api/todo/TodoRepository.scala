3ac16a70e922c22350c0f0080f5e08628e50cc89
kirill5k <immotional@aol.com>
2022-09-08 11:28:46 +0100

    Add mongo repo

--- api/src/main/scala/hellozio/api/todo/TodoRepository.scala
+++ api/src/main/scala/hellozio/api/todo/TodoRepository.scala
@@ -1,9 +1,16 @@
 package hellozio.api.todo
 
+import io.circe.Codec
 import hellozio.domain.common.errors.AppError
+import hellozio.domain.todo.Todo.Id
 import hellozio.domain.todo.{CreateTodo, Todo}
+import mongo4cats.bson.ObjectId
+import mongo4cats.circe.given
+import mongo4cats.operations.{Filter, Update}
+import mongo4cats.zio.{ZMongoCollection, ZMongoDatabase}
 
 import java.util.UUID
+import java.time.Instant
 import zio.*
 
 trait TodoRepository {
@@ -12,6 +19,47 @@ trait TodoRepository {
   def get(id: Todo.Id): IO[AppError, Todo]
   def getAll: IO[AppError, List[Todo]]
   def delete(id: Todo.Id): IO[AppError, Unit]
+}
+
+final private case class TodoEntity(_id: ObjectId, task: String, createdAt: Instant) derives Codec.AsObject
+
+final private case class TodoRepositoryMongo(collection: ZMongoCollection[TodoEntity]) extends TodoRepository {
+
+  def create(todo: CreateTodo): IO[AppError, Todo] =
+    val id = ObjectId.gen
+    collection
+      .insertOne(TodoEntity(id, todo.task.value, todo.createdAt))
+      .as(Todo(Todo.Id(id.toHexString), todo.task, todo.createdAt))
+      .mapError(t => AppError.Db(t.getMessage))
+
+  def update(todo: Todo): IO[AppError, Unit] =
+    collection
+      .updateOne(Filter.idEq(ObjectId(todo.id.value)), Update.set("task", todo.task.value).set("createdAt", todo.createdAt))
+      .mapError(t => AppError.Db(t.getMessage))
+      .flatMap(res => ZIO.cond(res.getMatchedCount > 0, (), AppError.TodoNotFound(todo.id)))
+
+  def get(id: Id): IO[AppError, Todo] =
+    collection
+      .find(Filter.idEq(ObjectId(id.value)))
+      .first
+      .mapError(t => AppError.Db(t.getMessage))
+      .flatMap {
+        case None    => ZIO.fail(AppError.TodoNotFound(id))
+        case Some(t) => ZIO.succeed(Todo(Todo.Id(t._id.toHexString), Todo.Task(t.task), t.createdAt))
+      }
+
+  def getAll: IO[AppError, List[Todo]] =
+    collection.find.all
+      .mapBoth(
+        t => AppError.Db(t.getMessage),
+        ts => ts.map(t => Todo(Todo.Id(t._id.toHexString), Todo.Task(t.task), t.createdAt)).toList
+      )
+
+  def delete(id: Id): IO[AppError, Unit] =
+    collection
+      .deleteOne(Filter.idEq(ObjectId(id.value)))
+      .mapError(t => AppError.Db(t.getMessage))
+      .flatMap(res => ZIO.cond(res.getDeletedCount > 0, (), AppError.TodoNotFound(id)))
 }
 
 final private case class TodoRepositoryInmemory(storage: Ref[Map[Todo.Id, Todo]]) extends TodoRepository {
@@ -43,6 +91,10 @@ final private case class TodoRepositoryInmemory(storage: Ref[Map[Todo.Id, Todo]]
 
 object TodoRepository {
   val inmemory: ULayer[TodoRepository] = ZLayer(Ref.make(Map.empty[Todo.Id, Todo]).map(TodoRepositoryInmemory.apply))
+  val mongo: URLayer[ZMongoDatabase, TodoRepository] = {
+    val collection = ZIO.serviceWithZIO[ZMongoDatabase](_.getCollectionWithCodec[TodoEntity]("todos"))
+    ZLayer.fromZIO(collection.map(TodoRepositoryMongo.apply).orDie)
+  }
 
   def create(todo: CreateTodo): ZIO[TodoRepository, AppError, Todo] = ZIO.serviceWithZIO[TodoRepository](_.create(todo))
   def getAll: ZIO[TodoRepository, AppError, List[Todo]]             = ZIO.serviceWithZIO[TodoRepository](_.getAll)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,6 +12,7 @@ object Dependencies {
     val fs2Kafka       = "3.0.0-M7"
     val kafka          = "3.2.0"
     val http4s         = "0.23.12"
+    val mongo4cats     = "0.6.1"
 
     val scalaTest = "3.2.12"
     val mockito   = "3.2.10.0"
@@ -21,6 +22,14 @@ object Dependencies {
     val pureconfig = "com.github.pureconfig" %% "pureconfig-core" % Versions.pureconfig
     val logback    = "ch.qos.logback"         % "logback-classic" % Versions.logback
     val fs2Kafka   = "com.github.fd4s"       %% "fs2-kafka"       % Versions.fs2Kafka
+
+    object mongo4cats {
+      val zio      = "io.github.kirill5k" %% "mongo4cats-zio"          % Versions.mongo4cats excludeAll(ExclusionRule(organization = "org.scala-lang.modules"))
+      val circe    = "io.github.kirill5k" %% "mongo4cats-circe"        % Versions.mongo4cats excludeAll(ExclusionRule(organization = "org.scala-lang.modules"))
+      val embedded = "io.github.kirill5k" %% "mongo4cats-zio-embedded" % Versions.mongo4cats % Test
+
+      val all = Seq(zio, circe, embedded)
+    }
 
     object http4s {
       val blazeServer = "org.http4s" %% "http4s-blaze-server" % Versions.http4s
@@ -60,7 +69,8 @@ object Dependencies {
     Libraries.logback,
     Libraries.http4s.blazeServer
   ) ++
-    Libraries.tapir.all
+    Libraries.tapir.all ++
+    Libraries.mongo4cats.all
 
   val consumer = Seq(
     Libraries.pureconfig,
